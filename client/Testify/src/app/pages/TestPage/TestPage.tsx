e0ee26174910e7b9debbcfe8cb71741a23e56a31
Илья Чапля <_ilchpl_@MacBook-Air-Ila.local>
2024-11-11 10:12:19 +0300

    test ui is done, server possibly done

--- client/Testify/src/app/pages/TestPage/TestPage.tsx
+++ client/Testify/src/app/pages/TestPage/TestPage.tsx
@@ -1,11 +1,47 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import styles from './TestPage.module.scss'
-
+import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { getTests } from "../../../api/tests/handlers";
+import { ITest } from "../../../types/ITest";
+import { TestItem } from "../../components/TestItem/TestItem";
+import { AnimatePresence } from "framer-motion";
 
 const TestPage: FC = () => {
+
+    const [step, setStep] = useState<number>(0)
+    const [result, setResult] = useState<number>(0)
+
+    const queryClient = useQueryClient()
+
+    const { data, isLoading } = useQuery<ITest[]>({queryKey: ["tests"], queryFn: getTests})
+
+    if (isLoading) {
+        return (
+            <div className={styles.loading}>
+                Загрузка...
+            </div>
+        )
+    }
+
+    if (step === data?.length) {
+        return (
+            <>Результат {result}/{data.length}</>
+        )
+    }
+
     return (
         <section className={styles.window}>
-            Тут будет тест
+            <AnimatePresence>
+                {data && <TestItem
+                            step={step} 
+                            setStep={setStep} 
+                            setResult={setResult} 
+                            result={result} 
+                            id={step} 
+                            correctAnswer={data[step].correctAnswer} 
+                            answers={data[step].answers}
+                            question={data[step].question}/>}
+            </AnimatePresence>
         </section>
     )
 }
